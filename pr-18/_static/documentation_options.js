4c787d16f0c58161c6fd7200b71ac53bc9be0620
timtreis <timtreis@users.noreply.github.com>
2026-08-19 02:27:05 +0000

    Preview for PR #18 74528407e0cdcdb8e729755fd0f8fdfadc2bcff5

--- pr-18/_static/documentation_options.js
+++ pr-18/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g375c17330.d20260818',
+    VERSION: '0.1.dev1+g375c17330.d20260819',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
